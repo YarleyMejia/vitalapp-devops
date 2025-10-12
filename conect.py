e3b98f0783e4649902fad12ac64eec7321c3d65d
YarleyMejia <100544029+YarleyMejia@users.noreply.github.com>
2025-10-12 12:40:02 -0500

    cambios de conexion de bases de datos atlas

---
+++ conect.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+# Reemplaza con tu cadena real de Atlas:
+uri = "mongodb+srv://ycmejia:Familia.967@vital.joik75f.mongodb.net/?retryWrites=true&w=majority&appName=vital"
+
+client = MongoClient(uri)
+
+# Accede a la base de datos
+db = client["saludvital"]
+
+# Ejemplo: mostrar las colecciones existentes
+print(db.list_collection_names())
+
+# Insertar un ejemplo
+db.usuarios.insert_one({"nombre": "Camilo", "rol": "Admin"})
+
+print("✅ Conexión exitosa y dato insertado")
